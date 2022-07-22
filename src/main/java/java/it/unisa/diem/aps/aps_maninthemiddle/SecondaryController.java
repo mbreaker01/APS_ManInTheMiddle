38381e2c8c96d31f74f60400a39651167ecbbce4
mbreaker <mario-boccia@hotmail.it>
2022-07-22 10:23:57 +0200

    changed package

---
+++ src/main/java/java/it/unisa/diem/aps/aps_maninthemiddle/SecondaryController.java
@@ -0,0 +1,12 @@
+package java.it.unisa.diem.aps.aps_maninthemiddle;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
