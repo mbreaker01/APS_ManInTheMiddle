3f15948fc0a8227568904242ce99f3a14daf0027
mbreaker <mario-boccia@hotmail.it>
2022-07-22 11:36:28 +0200

    added: Mixer, Presidente and Scrutinatore classes

---
+++ src/main/java/java/it/unisa/diem/aps/aps_maninthemiddle/Mixer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.it.unisa.diem.aps.aps_maninthemiddle;
+
+/**
+ *
+ * @author mario
+ */
+public class Mixer {
+    private static int cnt = 0;
+    
+    private int ID;
+
+    public Mixer() {
+        this.ID = cnt++;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    
+}
---
+++ src/main/java/java/it/unisa/diem/aps/aps_maninthemiddle/Scrutinatore.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.it.unisa.diem.aps.aps_maninthemiddle;
+
+/**
+ *
+ * @author mario
+ */
+public class Scrutinatore {
+    
+}
