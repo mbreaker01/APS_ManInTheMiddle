b6582b8f1c277367cc11e2579ce86e325874129e
simon <simon@172.19.193.187>
2022-07-22 12:16:48 +0200

    ParserTest complete

---
+++ src/main/java/java/it/unisa/diem/aps/aps_maninthemiddle/Scheda.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.it.unisa.diem.aps.aps_maninthemiddle;
+
+/**
+ *
+ * @author simon
+ */
+class Scheda {
+    
+    private Candidato voto;
+
+    public Scheda(Candidato voto) {
+        this.voto = voto;
+    }
+
+    
+    public Candidato getVoto() {
+        return voto;
+    }
+
+    public void setVoto(Candidato voto) {
+        this.voto = voto;
+    }
+    
+    
+}
